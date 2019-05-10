f123ae303f87dd3e56421af88849d82cf9720c9f
lydmgem <lydmgem@gmail.com>
2019-05-10 11:12:12 -0500

    getting a lot of error

---
+++ client/src/components/MenuAdmin/index.js
@@ -0,0 +1,1 @@
+export { MenuAdmin } from "./MenuAdmin.js";
--- client/src/pages/XPageTwo/XPageTwo.js
+++ client/src/pages/XPageTwo/XPageTwo.js
@@ -1,29 +1,30 @@
-import React, { Component } from "react";
-import API from "../../utils/API";
-import { Link } from "react-router-dom";
-import { XComponentOne, XComponentTwo } from '../../components/XComponent'
+// import React, { Component } from "react";
+// import API from "../../utils/API";
+// import { Link } from "react-router-dom";
+// import { XComponentOne, XComponentTwo } from '../../components/XComponent'
+// import { Jumbotron } from '../../components/Jumbotron'
 
-class XPageTwo extends Component {
-  state = {
-    array: ['X', 'PageTwo', 'Array'],
-    string: 'XPageTwoString',
-  };
+// class XPageTwo extends Component {
+//   state = {
+//     array: ['X', 'PageTwo', 'Array'],
+//     string: 'XPageTwoString',
+//   };
 
-  componentDidMount() {
-    API.getDocuments()
-      .then((res) => { console.log(res.data) })
-      .catch((err) => console.log(err));
-  }
+//   componentDidMount() {
+//     API.getDocuments()
+//       .then((res) => { console.log(res.data) })
+//       .catch((err) => console.log(err));
+//   }
 
-  render() {
-    return (<div>
-      <Link to={"/"}>
-        <strong>Go to XPageOne</strong>
-      </Link>
-      <XComponentOne array={this.state.array} string={this.state.string} />
-      <XComponentTwo array={this.state.array} string={this.state.string} />
-    </div>);
-  }
-}
+//   render() {
+//     return (<div>
+//       <Link to={"/"}>
+//         <strong>Go to XPageOne</strong>
+//       </Link>
+//       <XComponentOne array={this.state.array} string={this.state.string} />
+//       <XComponentTwo array={this.state.array} string={this.state.string} />
+//     </div>);
+//   }
+// }
 
-export default XPageTwo;
+// export default XPageTwo;
