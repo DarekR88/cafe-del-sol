e481add5d6651f4bafa7cb1903c1be85b0c1cfc1
lydmgem <lydmgem@gmail.com>
2019-05-10 17:03:13 -0500

    form and list divs added

--- client/src/components/AdminList/AdminList.js
+++ client/src/components/AdminList/AdminList.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { TabContent, TabPane, Nav, NavItem, NavLink, Card, Button, CardTitle, CardText, Row, Col } from 'reactstrap';
+import { TabContent, TabPane, Nav, NavItem, NavLink, Row, Col } from 'reactstrap';
 import classnames from 'classnames';
 
-export default class Example extends React.Component {
+class AdminList extends React.Component {
   constructor(props) {
     super(props);
 
@@ -21,48 +21,73 @@ export default class Example extends React.Component {
   }
   render() {
     return (
-      <div>
+      <div className='AdminList'>
         <Nav tabs>
           <NavItem>
             <NavLink
               className={classnames({ active: this.state.activeTab === '1' })}
               onClick={() => { this.toggle('1'); }}
             >
-              Tab1
+              Appetizer
             </NavLink>
           </NavItem>
+
           <NavItem>
             <NavLink
               className={classnames({ active: this.state.activeTab === '2' })}
               onClick={() => { this.toggle('2'); }}
             >
-              Moar Tabs
+              Lunch
+            </NavLink>
+          </NavItem>
+
+          <NavItem>
+            <NavLink
+              className={classnames({ active: this.state.activeTab === '3' })}
+              onClick={() => { this.toggle('3'); }}
+            >
+              Dinner
             </NavLink>
           </NavItem>
         </Nav>
+
         <TabContent activeTab={this.state.activeTab}>
           <TabPane tabId="1">
             <Row>
               <Col sm="12">
-                <h4>Tab 1 Contents</h4>
+                <ul className='app-ul'>
+                  <li className='app-list'>One</li>
+                  <li className='app-list'>Two</li>
+                  <li className='app-list'>Three</li>
+                </ul>
               </Col>
             </Row>
           </TabPane>
+        </TabContent>
+
+        <TabContent activeTab={this.state.activeTab}>
           <TabPane tabId="2">
             <Row>
-              <Col sm="6">
-                <Card body>
-                  <CardTitle>Special Title Treatment</CardTitle>
-                  <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-                  <Button>Go somewhere</Button>
-                </Card>
+              <Col sm="12">
+                <ul className='lunch-ul'>
+                  <li className='lunch-list'>One</li>
+                  <li className='lunch-list'>Two</li>
+                  <li className='lunch-list'>Three</li>
+                </ul>
               </Col>
-              <Col sm="6">
-                <Card body>
-                  <CardTitle>Special Title Treatment</CardTitle>
-                  <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-                  <Button>Go somewhere</Button>
-                </Card>
+            </Row>
+          </TabPane>
+        </TabContent>
+
+        <TabContent activeTab={this.state.activeTab}>
+          <TabPane tabId="3">
+            <Row>
+              <Col sm="12">
+                <ul className='dinner-ul'>
+                  <li className='dinner-list'>One</li>
+                  <li className='dinner-list'>Two</li>
+                  <li className='dinner-list'>Three</li>
+                </ul>
               </Col>
             </Row>
           </TabPane>
@@ -71,3 +96,5 @@ export default class Example extends React.Component {
     );
   }
 }
+
+export { AdminList };
--- client/src/pages/XPageTwo/XPageTwo.js
+++ client/src/pages/XPageTwo/XPageTwo.js
@@ -1,103 +1,76 @@
 import React, { Component } from "react";
 import API from "../../utils/API";
 import { Link } from "react-router-dom";
-import { XComponentOne, XComponentTwo } from '../../components/XComponent'
+import { XComponentOne, XComponentTwo } from '../../components/XComponent';
+import { AdminList } from '../../components/AdminList';
+import { Input, TextArea, FormBtn } from '../../components/Form';
+import './XPageTwo.css';
 
 class XPageTwo extends Component {
+  // Setting our component's initial state
   state = {
-    array: ['X', 'PageTwo', 'Array'],
-    string: 'XPageTwoString',
+    name: '',
+    description: '',
+    price: '',
+    image: '',
   };
 
-  componentDidMount() {
-    API.getDocuments()
-      .then((res) => { console.log(res.data) })
-      .catch((err) => console.log(err));
-  }
+  // componentDidMount() {
+  //   API.getDocuments()
+  //     .then((res) => { console.log(res.data) })
+  //     .catch((err) => console.log(err));
+  // }
 
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    if (this.state.name && this.state.price) {
+      // Need logic for if the Item they are adding is for Appetizer/Lunch/Dinner
+    }
+  }
   render() {
     return (<div>
       <Link to={"/"}>
         <strong>Go to XPageOne</strong>
       </Link>
-      <XComponentOne array={this.state.array} string={this.state.string} />
-      <XComponentTwo array={this.state.array} string={this.state.string} />
+      {/* <XComponentOne array={this.state.array} string={this.state.string} />
+      <XComponentTwo array={this.state.array} string={this.state.string} /> */}
+      <br />
+      <AdminList className='AdminList' />
+      <form className='Admin-Input'>
+        <Input
+          value={this.state.name}
+          onChange={this.handleInputChange}
+          name="name"
+          placeholder="Name (required)"
+        />
+        <Input
+          value={this.state.price}
+          onChange={this.handleInputChange}
+          name="price"
+          placeholder="Price (required)"
+        />
+        <TextArea
+          value={this.state.description}
+          onChange={this.handleInputChange}
+          name="description"
+          placeholder="Description (Optional)"
+        />
+        <FormBtn
+          disabled={!(this.state.name && this.state.price)}
+          onClick={this.handleFormSubmit}
+        >
+          Submit
+        </FormBtn>
+      </form>
     </div>);
   }
 }
 
 export default XPageTwo;
-
-import React from 'react';
-import { TabContent, TabPane, Nav, NavItem, NavLink, Card, Button, CardTitle, CardText, Row, Col } from 'reactstrap';
-import classnames from 'classnames';
-
-export default class Example extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      activeTab: '1'
-    };
-  }
-
-  toggle(tab) {
-    if (this.state.activeTab !== tab) {
-      this.setState({
-        activeTab: tab
-      });
-    }
-  }
-  render() {
-    return (
-      <div>
-        <Nav tabs>
-          <NavItem>
-            <NavLink
-              className={classnames({ active: this.state.activeTab === '1' })}
-              onClick={() => { this.toggle('1'); }}
-            >
-              Tab1
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink
-              className={classnames({ active: this.state.activeTab === '2' })}
-              onClick={() => { this.toggle('2'); }}
-            >
-              Moar Tabs
-            </NavLink>
-          </NavItem>
-        </Nav>
-        <TabContent activeTab={this.state.activeTab}>
-          <TabPane tabId="1">
-            <Row>
-              <Col sm="12">
-                <h4>Tab 1 Contents</h4>
-              </Col>
-            </Row>
-          </TabPane>
-          <TabPane tabId="2">
-            <Row>
-              <Col sm="6">
-                <Card body>
-                  <CardTitle>Special Title Treatment</CardTitle>
-                  <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-                  <Button>Go somewhere</Button>
-                </Card>
-              </Col>
-              <Col sm="6">
-                <Card body>
-                  <CardTitle>Special Title Treatment</CardTitle>
-                  <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-                  <Button>Go somewhere</Button>
-                </Card>
-              </Col>
-            </Row>
-          </TabPane>
-        </TabContent>
-      </div>
-    );
-  }
-}
